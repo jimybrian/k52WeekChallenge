f0ab21a509bb7c8891bcfb1904bca5a0bfe679ea
BrianKiwaa <briankiwaa@outlook.com>
2019-05-10 14:42:40 +0300

    Added the readme.md file

---
+++ app/src/androidTest/java/k/fiftytwochallenge/test/ChallengeRepoTest.kt
@@ -0,0 +1,28 @@
+package k.fiftytwochallenge.test
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ChallengeRepoTest {
+
+    @Test
+    fun setupInitialAmountAndWeeks() {
+    }
+
+    @Test
+    fun calcSaveTotals() {
+    }
+
+    @Test
+    fun calcSaveTotalsUpdateTextView() {
+    }
+
+    @Test
+    fun calcAndDisplay() {
+    }
+
+    @Test
+    fun searchForWeek() {
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+##Project
+This is is a simple project for the 52 week challenge
+The app uses Realm Database to persist data
